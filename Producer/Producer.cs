f25b0b5f33364cebf112a732f911ccd055c8e7d7
Varun Gupta <thevarungupta5@gmail.com>
2021-06-11 10:24:47 +0530

    Initial Commit

---
+++ Producer/Producer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection()){
+                using(var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(
+                        queue: "test",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    string message = "My first message";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "test",
+                                        basicProperties: null,
+                                        body: body);
+                    Console.WriteLine("Messag Send: {0}", message);
+                }
+            }
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
